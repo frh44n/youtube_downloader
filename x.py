12ed5aac6c425314f1b7754c29130b9498528d43
frh44n <107492548+frh44n@users.noreply.github.com>
2024-06-16 02:19:08 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
